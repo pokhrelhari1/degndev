4c3e8928c88f10907b3bbb24fbfaab7b51859633
ashutoshjak <58728879+ashutoshjak@users.noreply.github.com>
2020-07-01 21:23:17 +0545

    contact form revised

--- degndev/script.js
+++ degndev/script.js
@@ -18,19 +18,8 @@ function openNav() {
 
 }
 
-
-
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
-
-
-// function openNav() {
-//   document.getElementById("mySidenav").style.width = "250px";
-// }
-//
-// function closeNav() {
-//   document.getElementById("mySidenav").style.width = "0";
-// }
