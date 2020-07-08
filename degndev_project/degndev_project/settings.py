1a98e355c4bbeb6b604dc6af4b53b073af825467
Hari Pokhrel <pokhrelhari1@gmail.com>
2020-07-08 19:02:22 +0545

    backend added

--- degndev_project/degndev_project/settings.py
+++ degndev_project/degndev_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PortfolioConfig',
     'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- degndev_project/portfolio/models.py
+++ degndev_project/portfolio/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class portfolio(models.Model):
+    title = models.CharField(max_length=264)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
